330efd52f3623329d22b9eb5fbedd540d06ae592
tmznf963 <qhfhd963@naver.com>
2018-06-26 16:33:07 +0900

    update

--- nodeServerVimeo/router/admin/index.js
+++ nodeServerVimeo/router/admin/index.js
@@ -19,7 +19,7 @@ router.get("/mng",function(req,res){
 });
 
 //REST ful API
-// 1. /movie GET
+// 1. /admin GET
 router.get('/',function(req,res){
     var responseData ={};
 
@@ -36,7 +36,27 @@ router.get('/',function(req,res){
     });
 });
 
-// 3. /movie/:keyword , GET
+// 2. /admin , POST
+router.post('/', function(req,res){
+	var code = req.body.code;
+	var movie_title = req.body.movie_title;
+	var point = req.body.point;
+	var genres = req.body.Genres;
+  var times = req.body.Times;
+  var releases = req.body.Releases;
+  var directors = req.body.Directors;
+  var actors = req.body.Actors;
+  var grades = req.body.Grades;
+
+	var sql = {code,movie_title,point,genres,times,releases,directors,actors,grades};
+	var query = connection.query('insert into movie2 set ?', sql, function(err,rows) {
+		if(err) throw err
+		return res.json({'result' : 1});
+	})
+
+})
+
+// 3. /admin/:keyword , GET
 router.get('/:keyword', function(req,res) {
 	var keyword = req.params.keyword; //keyword == 사랑
   //console.log("keyword : ",keyword);
@@ -57,6 +77,24 @@ router.get('/:keyword', function(req,res) {
 	})
 })
 
+// 4. /admin/:movie_title , DELETE
+router.delete('/:movie_title', function(req,res) {
+	var movie_title = req.params.movie_title;
+
+	var responseData = {};
+
+	var query = connection.query('delete from movie2 where movie_title =?', [movie_title], function(err, rows) {
+		if(err) throw err;
+
+		if(rows.affectedRows > 0) {
+			responseData.result = 1;
+			responseData.data = movie_title;
+		} else {
+			responseData.result = 0;
+		}
+		res.json(responseData)
+	})
+})
 
 
 module.exports = router;
